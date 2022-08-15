34fab0ee362dce57b20c362e220f960031b7ad73
AnsonLoHK <chiaochiao1106@gmail.com>
2022-08-15 16:21:15 +0800

    在同一個路徑放兩個components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -6,6 +9,15 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login />}></Route>
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <Navbar />
+              <Dashboard />
+            </>
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
