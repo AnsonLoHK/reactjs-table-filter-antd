15f91b4f8182a7bcb1448bf2ce3d3615cbdc8c01
AnsonLoHK <chiaochiao1106@gmail.com>
2022-08-19 15:26:28 +0800

    引入第一筆假資料

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,62 @@
 import React from "react";
-import { GridComponent } from "@syncfusion/ej2-react-grids";
+import axios from "axios";
+import { Space, Table, Tag } from "antd";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const dataSource = [
+    {
+      key: "1",
+      name: "Barton",
+      age: 27,
+      address: "台北市",
+      tags: ["mod", "members"],
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+      render: (_, { tags }) => (
+        <>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+
+            if (tag === "loser") {
+              color = "volcano";
+            }
+
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+  ];
+  return (
+    <div>
+      <Table dataSource={dataSource} columns={columns} />;
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bulma/css/bulma.min.css";
+import "antd/dist/antd.min.css";
 import { ContextProvider } from "./contexts/ContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
