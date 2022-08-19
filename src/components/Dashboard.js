c6bb8109f458135d4a23d2fa44f70f9a1171e3a1
AnsonLoHK <chiaochiao1106@gmail.com>
2022-08-19 18:28:12 +0800

    1.計算 盈虧 2.遊戲類別篩選 3.啟用提用篩選 4.固定欄位

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,37 +1,75 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { Space, Table, Tag } from "antd";
-
+import { Space, Table, Tag, Typography } from "antd";
+const { Text } = Typography;
 // https://62ff3dbf9350a1e548da5ec5.mockapi.io/mockData
 const Dashboard = () => {
-  const [name, setName] = useState("Feza");
-  const [age, setAge] = useState(40);
+  const [name, setName] = useState("瘦子");
+  const [category, setCategory] = useState("Rummy");
   const [address, setAddress] = useState("台北市");
   const [tags, setTags] = useState(["停用"]);
+  const [income, setIncome] = useState(32750);
+  const [payment, setpayment] = useState(100);
   const [dataSource, setDataSource] = useState(null);
   const [state, setState] = useState({ error: null });
 
   const columns = [
     {
-      title: "ID",
+      title: "編號",
       dataIndex: "id",
       align: "center",
       fixed: "left",
       width: 100,
     },
     {
-      title: "Name",
+      title: "玩家",
       dataIndex: "name",
       key: "name",
+      align: "center",
       fixed: "left",
       width: 100,
     },
     {
-      title: "Age",
-      dataIndex: "age",
-      key: "age",
+      title: "營利",
+      dataIndex: "income",
+      key: "income",
       align: "center",
+    },
+    {
+      title: "虧損",
+      dataIndex: "payment",
+      key: "payment",
+      align: "center",
+    },
+    {
+      title: "遊戲類別",
+      dataIndex: "category",
+      key: "category",
+      align: "center",
+      filters: [
+        {
+          text: "武俠遊戲",
+          value: "武俠遊戲",
+        },
+        {
+          text: "LOL",
+          value: "LOL",
+        },
+        {
+          text: "跑跑卡丁車",
+          value: "跑跑卡丁車",
+        },
+        {
+          text: "Lottery Ada",
+          value: "Lottery Ada",
+        },
+        {
+          text: "Rummy",
+          value: "Rummy",
+        },
+      ],
+      onFilter: (value, record) => record.category.indexOf(value) === 0,
     },
     {
       title: "Address",
@@ -90,9 +128,11 @@ const Dashboard = () => {
   const postData = () => {
     axios.post(`https://62ff3dbf9350a1e548da5ec5.mockapi.io/mockData`, {
       name,
-      age,
+      category,
       address,
       tags,
+      income,
+      payment,
     });
   };
 
@@ -107,6 +147,8 @@ const Dashboard = () => {
       setState({ error });
     }
   }
+
+  console.log("dataSource", dataSource);
 
   if (state.error) {
     return <h1>{state.error.message}</h1>;
@@ -128,6 +170,37 @@ const Dashboard = () => {
         scroll={{
           x: 1500,
           y: 300,
+        }}
+        summary={(pageData) => {
+          let totalIncome = 0;
+          let totalPayment = 0;
+          pageData.forEach(({ income, payment }) => {
+            totalIncome += income;
+            totalPayment += payment;
+          });
+          return (
+            <>
+              <Table.Summary.Row align="center">
+                <Table.Summary.Cell index={0} colSpan={2}>
+                  各項總額
+                </Table.Summary.Cell>
+                <Table.Summary.Cell index={3}>
+                  <Text>{totalIncome}</Text>
+                </Table.Summary.Cell>
+                <Table.Summary.Cell index={3}>
+                  <Text type="danger">{totalPayment}</Text>
+                </Table.Summary.Cell>
+              </Table.Summary.Row>
+              <Table.Summary.Row align="center">
+                <Table.Summary.Cell index={0} colSpan={2}>
+                  損益盈餘
+                </Table.Summary.Cell>
+                <Table.Summary.Cell index={2} colSpan={2}>
+                  <Text type="danger">{totalIncome - totalPayment}</Text>
+                </Table.Summary.Cell>
+              </Table.Summary.Row>
+            </>
+          );
         }}
       />
       ;
