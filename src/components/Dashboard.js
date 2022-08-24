0dddd0fc98dac9c95f24537ec364251e2a253c1a
AnsonLoHK <chiaochiao1106@gmail.com>
2022-08-24 09:53:50 +0800

    刪掉不必要的code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { Space, Table, Tag, Typography, Button, Input } from "antd";
+import { Space, Table, Tag, Button, Input } from "antd";
 import DateRangePicker from "./DateRangePicker";
 import { useStateContext } from "../contexts/ContextProvider";
 import { SearchOutlined } from "@ant-design/icons";
 import Highlighter from "react-highlight-words";
 
-const { Text } = Typography;
 // https://62ff3dbf9350a1e548da5ec5.mockapi.io/mockData
 const Dashboard = () => {
   // 測試初始值
