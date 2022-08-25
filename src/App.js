05545f90520d943ce5cc8fa5f9f85696b5dd1e08
AnsonLoHK <chiaochiao1106@gmail.com>
2022-08-25 18:03:36 +0800

    將已引用的4種table功用，reademe加註解

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // import Login from "./components/Login";
 // import { Guard } from "./pages/Guard";
-// import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CrudExample from "./components/CrudExample";
 import { useStateContext } from "./contexts/ContextProvider";
-// import TableSearch from "./components/TableSearch";
-// import DateTableSearch from "./components/DateTableSearch";
-// import TablePagination from "./components/TablePagination";
-// import DateTimeForm from "./components/DateTimeForm";
+import TableSearch from "./components/TableSearch";
+import DateTableSearch from "./components/DateTableSearch";
 
 function setToken(userToken) {
   const getUserToken = JSON.stringify(userToken);
@@ -22,13 +20,20 @@ function App() {
   console.log("user", user);
   return (
     <BrowserRouter>
+      <h1>主題1</h1>
       <CrudExample />
-      {/* <Dashboard /> */}
       <hr />
-      {/* <TablePagination />
-      <DateTableSearch /> */}
-      {/* <DateTimeForm /> */}
-      {/* <TableSearch /> */}
+
+      <h1>主題2</h1>
+      <Dashboard />
+      <hr />
+
+      <h1>主題3</h1>
+      <DateTableSearch />
+      <hr />
+
+      <h1>主題4</h1>
+      <TableSearch />
 
       {/* {user?.loggedIn === false && (
         <Routes>
